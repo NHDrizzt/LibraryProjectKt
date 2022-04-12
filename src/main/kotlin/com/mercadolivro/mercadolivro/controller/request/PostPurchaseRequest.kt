cfe1fa7b0aaf8d92637cef078be0f087d1aa2c19
Rokol <biel_s.carvalho2@hotmail.com>
2022-04-12 14:38:28 -0300

    Purchase model

---
+++ src/main/kotlin/com/mercadolivro/mercadolivro/controller/request/PostPurchaseRequest.kt
@@ -0,0 +1,16 @@
+package com.mercadolivro.mercadolivro.controller.request
+
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Positive
+
+data class PostPurchaseRequest (
+    @field:NotNull
+    @field:Positive
+    val customerId: Int,
+
+    @field:NotNull
+    val booksId: Set<Int>
+
+    )
+
+
